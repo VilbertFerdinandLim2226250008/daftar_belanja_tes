1f904ae5954cafa7d1489ab9547e4a6b7d188353
asdasdasdasdas <asdasdasdasdasda@mhs.mdp.ac.id>
2024-03-21 09:21:12 +0700

    first commit

--- lib/services/shopping_services.dart
+++ lib/services/shopping_services.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -6,6 +8,22 @@ class ShoppingService {
       FirebaseDatabase.instance.ref().child('shopping_list');
 
   Stream<Map<String, String>> getShoppingList() {
-    return _database.onValue.map((event) => null)
+    return _database.onValue.map((event) {
+      final Map<String, String> items = {};
+      DataSnapshot snapshot = event.snapshot;
+      print('Snapshot data: ${snapshot.value}');
+
+      if(snapshot.value != null) {
+        Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic>;
+        values.forEach((key, value) {
+          items[] = value as String;
+        });
+      }
+
+      return items;
+    });
+  }
+  void addShoppingList(String itemName) {
+    _database.push().set({'name' : itemName});
   }
 }
